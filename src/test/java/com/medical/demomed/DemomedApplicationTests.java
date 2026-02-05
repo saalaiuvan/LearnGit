cd3f675aa38c0671e8c5cdd3fd6866ff9caa49e5
uvan <uvanoptimistic@gmail.com>
2026-02-05 15:46:50 +0530

    hello world

---
+++ src/test/java/com/medical/demomed/DemomedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medical.demomed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemomedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
